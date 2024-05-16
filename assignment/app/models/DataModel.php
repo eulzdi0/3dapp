b18ec8b894012cf8f92aeeff8fb158a867f0925f
eulzdi0 <159737721+eulzdi0@users.noreply.github.com>
2024-05-16 13:32:10 +0100

    Add files via upload

---
+++ assignment/app/models/DataModel.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'BaseModel.php';
+
+class DataModel extends BaseModel {
+    public function __construct() {
+        parent::__construct();
+    }
+
+
+	
+	public function getImgs(){
+		return $this->getAll('imgs');
+	}
+	public function getInfos(){
+		return $this->getAll('info');
+	}
+	public function getBrands(){
+		return $this->getAll('brand');
+	}
+	public function getModels(){
+		return $this->getAll('models');
+	}
+}
+
+
+?>
