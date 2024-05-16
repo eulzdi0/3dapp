93777c905d96f18c61d41871020837cfbfcc4b64
eulzdi0 <159737721+eulzdi0@users.noreply.github.com>
2024-05-16 13:39:55 +0100

    Add files via upload

---
+++ assignment/public/index.php
@@ -0,0 +1,12 @@
+<?php
+// Load the core application
+require_once '../core/Database.php';
+require_once '../app/controllers/BaseController.php';
+
+// Instantiate the BaseController
+$controller = new BaseController();
+
+// Route the request
+$controller->handleRequest();
+
+?>
